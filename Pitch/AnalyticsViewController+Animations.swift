db95819066c52a9f458798fd9da5f2cecde5b30a
Daniel Kuntz <dkuntz0@gmail.com>
2016-12-24 12:44:59 -0500

    Put animation code for AnalyticsViewController in a separate file

--- Pitch/AnalyticsViewController+Animations.swift
+++ Pitch/AnalyticsViewController+Animations.swift
@@ -11,4 +11,42 @@ import UIKit
 
 extension AnalyticsViewController {
     
+    // MARK: - Animation
+    
+    func prepareForAnimation() {
+        scoreCircle.transform = CGAffineTransform(scaleX: 0, y: 0)
+        scoreCircle.alpha = 0.0
+        scoreLabel.format = "%d"
+        scoreLabel.text = "0"
+        
+        todayLabel.alpha = 0.0
+        todaySeparator.alpha = 0.0
+        descriptionLabel.alpha = 0.0
+        
+        todayLabelTopConstraint.constant += 250
+        todaySeparatorTopConstraint.constant += 350
+        descriptionLabelTopConstraint.constant += 450
+    }
+    
+    func animateIn() {
+        let score = UserDefaults.standard.today().inTunePercentage.roundTo(places: 2) * 100
+        self.scoreLabel.countFromZero(to: CGFloat(score), withDuration: 1.2)
+        
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.01, options: [.curveEaseInOut], animations: {
+            self.scoreCircle.transform = CGAffineTransform(scaleX: 1, y: 1)
+            self.scoreCircle.alpha = 1.0
+        }, completion: nil)
+        
+        todayLabelTopConstraint.constant -= 250
+        todaySeparatorTopConstraint.constant -= 350
+        descriptionLabelTopConstraint.constant -= 450
+        
+        UIView.animate(withDuration: 1.0, delay: 0.6, usingSpringWithDamping: 1.2, initialSpringVelocity: 0.1, options: [.curveEaseOut], animations: {
+            self.view.layoutIfNeeded()
+            self.todayLabel.alpha = 1.0
+            self.todaySeparator.alpha = 1.0
+            self.descriptionLabel.alpha = 1.0
+        }, completion: nil)
+    }
+    
 }
