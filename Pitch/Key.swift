99a818a7ef573ab5d6272c6b4d11bff1d0a0dc66
Daniel Kuntz <dkuntz0@gmail.com>
2017-01-06 10:55:52 -0500

    Syntax fix

--- Pitch/Key.swift
+++ Pitch/Key.swift
@@ -25,7 +25,7 @@ enum Key: Int {
     static let allCases: [Key] = [.fsharp, .g, .gsharp, .a, .asharp, .b, .c, .csharp, .d, .dsharp, .e, .f]
     
     static func fromName(_ name: String) -> Key? {
-        return = allCases.filter { key in
+        return allCases.filter { key in
             return name == key.sharpName || name == key.flatName
         }.first
     }
