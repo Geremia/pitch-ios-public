7a0ab18d423af506bb48860c81cba1799eaa24bf
Daniel Kuntz <dkuntz0@gmail.com>
2017-01-06 10:54:46 -0500

    Fixed a bug that caused the pitch pipe button titles to get screwed up.

--- Pitch/Key.swift
+++ Pitch/Key.swift
@@ -25,9 +25,14 @@ enum Key: Int {
     static let allCases: [Key] = [.fsharp, .g, .gsharp, .a, .asharp, .b, .c, .csharp, .d, .dsharp, .e, .f]
     
     static func fromName(_ name: String) -> Key? {
-        return allCases.filter { key in
+        let key = allCases.filter { key in
+            print(key.sharpName)
+            print(key.flatName)
             return name == key.sharpName || name == key.flatName
-        }.first
+        }
+        print(name)
+        print(key)
+        return key.first
     }
     
     var concertOffset: Int {
