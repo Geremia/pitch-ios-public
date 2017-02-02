acc8d74f54c83a59ec040a84f727e7066dae847c
Daniel Kuntz <dkuntz0@gmail.com>
2017-02-01 19:23:29 -0500

    Adjusted in-tune color to be less abrasive.

--- Pitch/UIColor+PitchColors.swift
+++ Pitch/UIColor+PitchColors.swift
@@ -11,7 +11,7 @@ import UIKit
 
 extension UIColor {
     @nonobjc static let almostInTune: UIColor = UIColor(red: 204/255.0, green: 255/255.0, blue: 229/255.0, alpha: 1.0)
-    @nonobjc static let inTune: UIColor = UIColor(red: 0/255.0, green: 255/255.0, blue: 125/255.0, alpha: 1.0)
+    @nonobjc static let inTune: UIColor = UIColor(red: 76/255.0, green: 217/255.0, blue: 100/255.0, alpha: 1.0)
     @nonobjc static let darkAlmostInTune: UIColor = UIColor(red: 0/255.0, green: 51/255.0, blue: 25/255.0, alpha: 1.0)
     @nonobjc static let darkInTune: UIColor = UIColor(red: 0/255.0, green: 179/255.0, blue: 86/255.0, alpha: 1.0)
     
