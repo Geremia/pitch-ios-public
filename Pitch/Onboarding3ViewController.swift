7711fd9edd23b863d3336df1dbfb6e3b50073fd7
Daniel Kuntz <dkuntz0@gmail.com>
2017-02-25 18:00:12 -0500

    Setup notifications permission onboarding screen.

--- Pitch/Onboarding3ViewController.swift
+++ Pitch/Onboarding3ViewController.swift
@@ -7,24 +7,26 @@
 //
 
 import UIKit
+import UserNotifications
 
 class Onboarding3ViewController: OnboardingViewController {
 
-    // MARK: - Setup Views
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
     // MARK: - Actions
     
     @IBAction func allowAccessPressed(_ sender: Any) {
-        
+        requestNotificationsPermission()
+    }
+    
+    func requestNotificationsPermission() {
+        let options: UNAuthorizationOptions = [.alert, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: options, completionHandler: { granted, error in
+            if granted {
+                self.performSegue(withIdentifier: "onboarding34", sender: nil)
+            }
+        })
     }
     
     @IBAction func noThanksPressed(_ sender: Any) {
-        
+        performSegue(withIdentifier: "onboarding34", sender: nil)
     }
 }
