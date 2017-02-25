a9b2fc87cf128de2511331d9a001f259be14c3bc
Daniel Kuntz <dkuntz0@gmail.com>
2017-02-25 18:07:55 -0500

    Setup final onboarding screen.

--- Pitch/Onboarding5ViewController.swift
+++ Pitch/Onboarding5ViewController.swift
@@ -22,8 +22,32 @@ class Onboarding5ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        styleLabel()
+    }
+    
+    func styleLabel() {
+        let boldFont = UIFont(name: "Lato-Regular", size: 22.0)!
+        let lightFont = UIFont(name: "Lato-Light", size: 22.0)!
+        
+        let chunk1 = NSAttributedString(string: "Leave Pitch open during your ", attributes: [NSFontAttributeName: lightFont])
+        let chunk2 = NSAttributedString(string: "entire practice session. ", attributes: [NSFontAttributeName: boldFont])
+        let chunk3 = NSAttributedString(string: "We'll let you know when your ", attributes: [NSFontAttributeName: lightFont])
+        let chunk4 = NSAttributedString(string: "daily analytics ", attributes: [NSFontAttributeName: boldFont])
+        let chunk5 = NSAttributedString(string: "are ready!", attributes: [NSFontAttributeName: lightFont])
+        
+        let combined: NSMutableAttributedString = chunk1 as! NSMutableAttributedString
+        combined.append(chunk2)
+        combined.append(chunk3)
+        combined.append(chunk4)
+        combined.append(chunk5)
+        
+        label.attributedText = combined
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.transitioningDelegate = self
     }
 }
 
