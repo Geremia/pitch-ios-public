a4149924a55b106d02ab9077f42c089988b89145
Daniel Kuntz <dkuntz0@gmail.com>
2017-02-25 16:34:47 -0500

    Added UIViewController subclasses in files for each onboarding screen

---
+++ Pitch/Onboarding1ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Onboarding1ViewController.swift
+//  Pitch
+//
+//  Created by Daniel Kuntz on 2/25/17.
+//  Copyright © 2017 Plutonium Apps. All rights reserved.
+//
+
+import UIKit
+
+class Onboarding1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
