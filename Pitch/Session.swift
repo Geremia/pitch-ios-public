05678ee7ff007018ce13f31660ec2f89a18464c2
Daniel Kuntz <dkuntz0@gmail.com>
2017-03-02 21:54:11 -0500

    Created a Session class and reorganized some stuff.

---
+++ Pitch/Session.swift
@@ -0,0 +1,18 @@
+//
+//  Session.swift
+//  Pitch
+//
+//  Created by Daniel Kuntz on 3/2/17.
+//  Copyright © 2017 Plutonium Apps. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+typealias SessionAnalytics = Day
+
+class Session: Object {
+    
+    // MARK: - Variables
+    
+}
