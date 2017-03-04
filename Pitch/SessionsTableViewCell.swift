1968f70dd9e884cda4ef700dd8ffc69037ff04fe
Daniel Kuntz <dkuntz0@gmail.com>
2017-03-03 19:19:10 -0500

    Added a SessionsTableViewCell class and hooked up outlets

---
+++ Pitch/SessionsTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  SessionsTableViewCell.swift
+//  Pitch
+//
+//  Created by Daniel Kuntz on 3/3/17.
+//  Copyright © 2017 Plutonium Apps. All rights reserved.
+//
+
+import UIKit
+
+class SessionsTableViewCell: UITableViewCell {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var timePassedLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var timeLeftLabel: UILabel!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var analyticsButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    // MARK: - Setup
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    // MARK: - Selected State
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
