be18ab7c74dece79d7ae27cadc6e3711ac34df4b
Daniel Kuntz <dkuntz0@gmail.com>
2017-03-04 22:42:29 -0500

    Integrated file naming into recorder class.

--- Pitch/Recorder.swift
+++ Pitch/Recorder.swift
@@ -16,23 +16,24 @@ class Recorder: NSObject {
     var file: AKAudioFile?
     var recorder: AKNodeRecorder?
     
-    private override init() {
-        let dirPaths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let url = dirPaths[0].appendingPathComponent("sound.caf")
-        let settings: [String : Any] = [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
-                                              AVEncoderBitRateKey: 16,
-                                              AVNumberOfChannelsKey: 2,
-                                              AVSampleRateKey: 44100.0]
-        
-        file = try? AKAudioFile(forWriting: url, settings: settings)
-        recorder = try? AKNodeRecorder(node: Tuner.sharedInstance.microphone, file: file)
-    }
+    private override init() {}
     
     private func newFileName() -> String {
         let number = UserDefaults.standard.fileNumber()
+        return "file\(number).caf"
     }
     
     func startRecording() {
+        let dirPaths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = dirPaths[0].appendingPathComponent(newFileName())
+        let settings: [String : Any] = [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+                                        AVEncoderBitRateKey: 16,
+                                        AVNumberOfChannelsKey: 2,
+                                        AVSampleRateKey: 44100.0]
+        
+        file = try? AKAudioFile(forWriting: url, settings: settings)
+        recorder = try? AKNodeRecorder(node: Tuner.sharedInstance.microphone, file: file)
+        
         try? recorder?.record()
     }
     
@@ -40,4 +41,14 @@ class Recorder: NSObject {
         try? recorder?.stop()
     }
     
+    func deleteCurrentRecording() {
+        if let file = file {
+            try? FileManager.default.removeItem(at: file.url)
+        }
+    }
+    
+    func reset() {
+        file = nil
+        recorder = nil
+    }
 }
