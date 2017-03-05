778ced4efa0bf19a4b683e275f5689e5b0ed5559
Daniel Kuntz <dkuntz0@gmail.com>
2017-03-04 22:28:08 -0500

    Made a recorder class with some basic stuff

---
+++ Pitch/Recorder.swift
@@ -0,0 +1,39 @@
+//
+//  Recorder.swift
+//  Pitch
+//
+//  Created by Daniel Kuntz on 3/4/17.
+//  Copyright © 2017 Plutonium Apps. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class Recorder: NSObject {
+    
+    static let sharedInstance: Recorder = Recorder()
+    
+    var file: AKAudioFile?
+    var recorder: AKNodeRecorder?
+    
+    private override init() {
+        let dirPaths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = dirPaths[0].appendingPathComponent("sound.caf")
+        let settings: [String : Any] = [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+                                              AVEncoderBitRateKey: 16,
+                                              AVNumberOfChannelsKey: 2,
+                                              AVSampleRateKey: 44100.0]
+        
+        file = try? AKAudioFile(forWriting: url, settings: settings)
+        recorder = try? AKNodeRecorder(node: Tuner.sharedInstance.microphone, file: file)
+    }
+    
+    func startRecording() {
+        try? recorder?.record()
+    }
+    
+    func stopRecording() {
+        try? recorder?.stop()
+    }
+    
+}
