a486bf5d2dffc68197c047ab183793a327593e17
Daniel Kuntz <dkuntz0@gmail.com>
2017-03-16 14:23:07 -0400

    Updated version updates text.

--- Xcode Project/Pitch/Constants.swift
+++ Xcode Project/Pitch/Constants.swift
@@ -11,4 +11,19 @@ import Foundation
 class Constants {
     static var pitchPipeIsPlayingSound: Bool = false
     static var currentOrientation: Orientation = .portrait
+    
+    static let versionUpdates: String = "Lots of awesome new stuff in this update!\n\n"
+        
+                                                + "- The interface has been cleaned up a bit! Just swipe to get to different screens.\n"
+                                                + "- NEW FEATURE: Sessions! Swipe over all the way over to the right and press the + to record your first session! After you're done, you can view Analytics or play back the recording.\n"
+                                                + "- Fixed a bug that caused Analytics data not to update.\n"
+                                                + "- Lots of performance improvements (nice!)\n\n"
+        
+                                                + "~~~~ COMING SOON ~~~~\n"
+                                                + "- Game center integration! Compete with friends for who has the best intonation.\n"
+                                                + "- Daily goals: know the one thing you should focus on to get a little better every day.\n\n"
+        
+                                                + "As always, if you have any comments, questions, or feedback don't be shy! I'd love to hear from you. Just press one of the 'Leave Feedback' buttons in the app. I always read and reply to all emails!\n\n"
+        
+                                                + "Thanks for being an awesome musician :)"
 }
